ddc618f478c66141f75b5d82cb5e56a0481594f3
manikandan <manikandan110305@gmail.com>
2025-06-27 21:34:57 +0530

    new vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/carRent/', // <-- ADD THIS LINE
+  base: '/carRental/', // <-- ADD THIS LINE
   plugins: [react(), tailwindcss()],
 })
